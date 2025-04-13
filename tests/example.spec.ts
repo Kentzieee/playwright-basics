6150b49481c243f9ab9270792b18a4fc9d75f3f6
Kentzie <Iamkentzielim@gmail.com>
2025-04-13 23:07:26 +0800

    Playwright Test Automation - Session 2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,15 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+// TEST = TEST CASE
+// ARRANGE -> ACT -> ASSERT
+// PW TEST RUN
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
+  
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
 
