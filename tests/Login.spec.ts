dd324aa215e4830db52092e197f19a0fc33015df
Kentzie <Iamkentzielim@gmail.com>
2025-04-14 20:46:22 +0800

    Added testing for locked_out_user

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,19 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-// Login using valid username and password
+// Login using standard_user
 test('Should successfully login with valid username and password', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
   await expect(page.locator('[data-test="item-4-title-link"] [data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
-  await page.screenshot({path: 'test-screenshots/login-success.png', fullPage: true})
+  await page.screenshot({path: 'test-screenshots/login-success-standard_user.png', fullPage: true})
 
   // Logout
   await page.getByRole('button', { name: 'Open Menu' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
   await expect(page.getByText('Swag Labs')).toBeVisible();
   await page.screenshot({path: 'test-screenshots/logout-success.png', fullPage: true})
+
+  // Login using locked_out_user
+  test('Should successfully login with valid username and password', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await expect(page.locator('[data-test="item-4-title-link"] [data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+    await page.screenshot({path: 'test-screenshots/login-success-locked_out_user.png', fullPage: true})
+
 });
 
   // Login using invalid username and password
