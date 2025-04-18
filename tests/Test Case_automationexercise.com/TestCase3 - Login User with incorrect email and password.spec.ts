d5011cbe25b307cb35f47f9bcf3bd0a864084846
Kentzie <Iamkentzielim@gmail.com>
2025-04-18 13:26:21 +0800

    Added TestCase3

---
+++ tests/Test Case_automationexercise.com/TestCase3 - Login User with incorrect email and password.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test'; 
+
+test('Register User', async ({ page }) => {
+
+// Navigate to url 'http://automationexercise.com'
+await page.goto('https://automationexercise.com/');
+
+// Verify that home page is visible successfully
+expect(page.getByText('Home')).toBeVisible();
+
+// Click on 'Signup / Login' button
+await page.locator('a', {hasText: "Signup / Login"}).click();
+
+// Verify 'Login to your account' is visible
+expect(page.getByText('Login to your account')).toBeVisible();
+
+// Enter incorrect email address and password
+await page.locator('[data-qa="login-email"]').fill('Incorrectusername@gmail.com');
+await page.locator('[data-qa="login-password"]').fill('Incorrectpassword');
+
+// Click 'login' button
+await page.locator('[data-qa="login-button"]').click();
+
+// Verify error 'Your email or password is incorrect!' is visible
+await expect(page.getByText('Your email or password is incorrect!')).toBeVisible();
+
+
+});
